2f4fdd91e32ca60efbbaee95866b5c7db5507c1f
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-15 20:50:27 +0700

    feat: add validation if user input wrong data type and update README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Golang 4
+
+This minitask have a goal to create terminal program to convert celcius temperature into fahrenheit, reamure adn kelvin
+
+## How to Use
+
+Run the application insert celcius number and chosse where you want to convert
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	. "golang4/lib"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -13,50 +14,56 @@ func main() {
 		}
 	}()
 
-	var number int
-	fmt.Println("\nmasukan dulu angka")
-	_, err := fmt.Scanln(&number)
-	if err != nil {
-		fmt.Println(err)
-		main()
-	}
-
-	fmt.Println("Pilih Konversi")
-	ShowMenu()
-
-	for true {
-		var input int
-		_, err := fmt.Scanln(&input)
-		fmt.Println(err)
+	for {
+		var test string
+		fmt.Println("\nmasukan dulu angka")
+		_, err := fmt.Scanln(&test)
+		number, err := strconv.Atoi(test)
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("HANYA ANGKA YANG DIPERBOLEHKAN!!!")
 			main()
 		} else {
-			switch input {
-			case 1:
-				// ( °C × 9/5) + 32 = 32 °F
-				result := number*(9/5) + 32
-				fmt.Print("\nHasilnya ", result)
-				fmt.Printf("\n\nJika ingin konversi lagi silahkan masukan angka lalu masukan angka 0 \n")
+		}
+
+		fmt.Println("Pilih Konversi")
+		ShowMenu()
+
+		for true {
+			var rawInput string
+			_, err := fmt.Scanln(&rawInput)
+			input, err := strconv.Atoi(rawInput)
+
+			if err != nil {
+				fmt.Println("angaka woy")
 				main()
-			case 2:
-				// °C * (4/5) = 8°Ré
-				result := number * 4 / 5
-				fmt.Print("Hasilnya ", result)
-				fmt.Printf("\n\nJika ingin konversi lagi silahkan pilih jika tidak masukan angka 0 \n")
-				main()
-			case 3:
-				// °C + 273.15 = 293.15 K
-				result := number + 273
-				fmt.Print("Hasilnya ", result)
-				fmt.Printf("\n\nJika ingin konversi lagi silahkan pilih jika tidak masukan angka 0 \n")
-				main()
-			case 0:
-				fmt.Println("Bay bay samapai jumpah")
-				os.Exit(0)
-				// default:
-				// 	fmt.Println("Pilihan tidak ada pilih yang benar")
+			} else {
+				switch input {
+				case 1:
+					// ( °C × 9/5) + 32 = 32 °F
+					result := number*(9/5) + 32
+					fmt.Print("\nHasilnya ", result)
+					fmt.Printf("\n\nJika ingin konversi lagi silahkan masukan angka lalu masukan angka 0 \n")
+					main()
+				case 2:
+					// °C * (4/5) = 8°Ré
+					result := number * 4 / 5
+					fmt.Print("Hasilnya ", result)
+					fmt.Printf("\n\nJika ingin konversi lagi silahkan pilih jika tidak masukan angka 0 \n")
+					main()
+				case 3:
+					// °C + 273.15 = 293.15 K
+					result := number + 273
+					fmt.Print("Hasilnya ", result)
+					fmt.Printf("\n\nJika ingin konversi lagi silahkan pilih jika tidak masukan angka 0 \n")
+					main()
+				case 0:
+					fmt.Println("Bay bay samapai jumpah")
+					os.Exit(0)
+					// default:
+					// 	fmt.Println("Pilihan tidak ada pilih yang benar")
+				}
+
 			}
 
 		}
